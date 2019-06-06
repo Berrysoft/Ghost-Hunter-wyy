d7d810f8ff1512fea8f0d5533cf51e362c5ea735
littlewu2508 <xgreenlandforwyy@gmail.com>
2019-06-06 14:56:45 +0800

    matlab代码

---
+++ transfer.py
@@ -0,0 +1,14 @@
+import numpy as np, h5py
+fipt = "answerfile.h5"
+fopt = "submissionfile.h5"
+
+opd = [('EventID', '<i8'), ('ChannelID', '<i2'),
+       ('PETime', 'f4'), ('Weight', 'f4')]
+
+with h5py.File(fipt) as ipt, h5py.File(fopt, "w") as opt:
+        rst = np.zeros(len(ipt['PETime'][0]), dtype=opd)
+        rst['PETime'] = ipt['PETime'][0]
+        rst['Weight'] = (ipt['Weight'][0])
+        rst['EventID'] = ipt['EventID'][0]
+        rst['ChannelID'] = ipt['ChannelID'][0]
+        opt.create_dataset('Answer', data=rst, compression='gzip')
