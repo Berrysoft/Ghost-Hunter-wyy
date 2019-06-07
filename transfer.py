9d525ab689621973b2a8bcf3097d16b8a259022b
littlewu2508 <xgreenlandforwyy@gmail.com>
2019-06-07 10:29:29 +0800

    给transfer.py添加说明

--- transfer.py
+++ transfer.py
@@ -5,7 +5,7 @@ fopt = "submissionfile.h5"
 opd = [('EventID', '<i8'), ('ChannelID', '<i2'),
        ('PETime', 'f4'), ('Weight', 'f4')]
 
-with h5py.File(fipt) as ipt, h5py.File(fopt, "w") as opt:
+with h5py.File(fipt) as ipt, h5py.File(fopt, "w") as opt: #将matlab生成的answerfile.h5文件（未压缩且格式不符合提交要求）转化为可供提交的h5文件
         rst = np.zeros(len(ipt['PETime'][0]), dtype=opd)
         rst['PETime'] = ipt['PETime'][0]
         rst['Weight'] = (ipt['Weight'][0])
